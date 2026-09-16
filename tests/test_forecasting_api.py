883a06aa125670e8d340601cfc990b589ba7e86e
etsioumpri <etsioumpri@gmail.com>
2026-09-16 14:50:37 +0100

    input validation tests done

--- tests/test_forecasting_api.py
+++ tests/test_forecasting_api.py
@@ -5,7 +5,8 @@ import requests
 
 from conftest import valid_request
 
-
+############################### API AVAILABILITY ##################################################
+print("API AVAILABILITY TESTS")
 def test_api_is_reachable(api_url, api_headers, valid_request):
     print("Testing that the forcasting API is accessible and does not return a server side error")
 
@@ -18,6 +19,8 @@ def test_api_is_reachable(api_url, api_headers, valid_request):
 
     assert response.status_code < 500
 
+############################### CORE FUNCTIONALITIES ################################################
+print("CORE FUNCTIONALITY TESTS")
 def test_valid_request_produces_successful_response(api_url, api_headers, valid_request):
     print("Testing that a valid request produces a successful response")
 
@@ -217,3 +220,197 @@ def test_identical_requests_produce_identical_responses(api_url, api_headers, va
     data_2 = response_2.json()
 
     assert data_1["demand_forecast"] == data_2["demand_forecast"]
+
+############################### INPUT VALIDATION ####################################################
+print("INPUT VALIDATION TESTS")
+def test_request_without_meter_produces_appropriate_response(api_url, api_headers, valid_request):
+    print("Testing that requests with missing field 'meter' produce the appropriate response")
+
+    url = f"{api_url}/forecast/gd_lic"
+
+    invalid_request = valid_request.copy()
+    invalid_request.pop("meter")
+
+    response = requests.post(url,
+                             json=invalid_request,
+                             headers=api_headers,
+                             timeout=60)
+
+    assert response.status_code == 422
+
+def test_request_without_site_produces_appropriate_response(api_url, api_headers, valid_request):
+    print("Testing that requests with missing field 'site' produce the appropriate response")
+
+    url = f"{api_url}/forecast/gd_lic"
+
+    invalid_request = valid_request.copy()
+    invalid_request.pop("site")
+
+    response = requests.post(url,
+                             json=invalid_request,
+                             headers=api_headers,
+                             timeout=60)
+
+    assert response.status_code == 422
+
+def test_request_without_start_time_produces_appropriate_response(api_url, api_headers, valid_request):
+    print("Testing that requests with missing field 'start_time' produce the appropriate response")
+
+    url = f"{api_url}/forecast/gd_lic"
+
+    invalid_request = valid_request.copy()
+    invalid_request.pop("start_time")
+
+    response = requests.post(url,
+                             json=invalid_request,
+                             headers=api_headers,
+                             timeout=60)
+
+    assert response.status_code == 422
+
+def test_request_with_empty_meter_field_produces_appropriate_response(api_url, api_headers, valid_request):
+    print("Testing that requests with empty 'meter' field produce the appropriate response")
+
+    url = f"{api_url}/forecast/gd_lic"
+
+    invalid_request = valid_request.copy()
+    invalid_request["meter"] = None
+
+    response = requests.post(url,
+                             json=invalid_request,
+                             headers=api_headers,
+                             timeout=60)
+
+    assert response.status_code == 422
+
+def test_request_with_empty_site_field_produces_appropriate_response(api_url, api_headers, valid_request):
+    print("Testing that requests with empty 'site' field produce the appropriate response")
+
+    url = f"{api_url}/forecast/gd_lic"
+
+    invalid_request = valid_request.copy()
+    invalid_request["site"] = None
+
+    response = requests.post(url,
+                             json=invalid_request,
+                             headers=api_headers,
+                             timeout=60)
+
+    assert response.status_code == 422
+
+def test_request_with_empty_start_time_field_produces_appropriate_response(api_url, api_headers, valid_request):
+    print("Testing that requests with empty 'start_time' field produce the appropriate response")
+
+    url = f"{api_url}/forecast/gd_lic"
+
+    invalid_request = valid_request.copy()
+    invalid_request["start_time"] = None
+
+    response = requests.post(url,
+                             json=invalid_request,
+                             headers=api_headers,
+                             timeout=60)
+
+    assert response.status_code == 422
+
+def test_request_with_invalid_meter_type_produces_appropriate_response(api_url, api_headers, valid_request):
+    print("Testing that requests with invalid 'meter' type produce the appropriate response")
+
+    url = f"{api_url}/forecast/gd_lic"
+
+    invalid_request = valid_request.copy()
+    invalid_request["meter"] = ["invalid_type"]
+
+    response = requests.post(url,
+                             json=invalid_request,
+                             headers=api_headers,
+                             timeout=60)
+
+    assert response.status_code == 422
+
+def test_request_with_invalid_site_type_produces_appropriate_response(api_url, api_headers, valid_request):
+    print("Testing that requests with invalid 'site' type produce the appropriate response")
+
+    url = f"{api_url}/forecast/gd_lic"
+
+    invalid_request = valid_request.copy()
+    invalid_request["site"] = ["invalid_type"]
+
+    response = requests.post(url,
+                             json=invalid_request,
+                             headers=api_headers,
+                             timeout=60)
+
+    assert response.status_code == 422
+
+def test_request_with_invalid_start_time_type_produces_appropriate_response(api_url, api_headers, valid_request):
+    print("Testing that requests with invalid 'start_time' type produce the appropriate response")
+
+    url = f"{api_url}/forecast/gd_lic"
+
+    invalid_request = valid_request.copy()
+    invalid_request["start_time"] = ["invalid_type"]
+
+    response = requests.post(url,
+                             json=invalid_request,
+                             headers=api_headers,
+                             timeout=60)
+
+    assert response.status_code == 422
+
+def test_request_with_unsupported_meter_value_produces_appropriate_response(api_url, api_headers, valid_request):
+    print("Testing that requests with unsupported 'meter' value produce the appropriate response")
+
+    url = f"{api_url}/forecast/gd_lic"
+
+    invalid_request = valid_request.copy()
+    invalid_request["meter"] = "unsupported_meter_name"
+
+    response = requests.post(url,
+                             json=invalid_request,
+                             headers=api_headers,
+                             timeout=60)
+
+    assert response.status_code == 400
+
+    data = response.json()
+
+    assert data["detail"] == f"Meter id {invalid_request['meter']} not valid for site {invalid_request['site']}"
+
+def test_request_with_unsupported_site_value_produces_appropriate_response(api_url, api_headers, valid_request):
+    print("Testing that requests with unsupported 'site' value produce the appropriate response")
+
+    url = f"{api_url}/forecast/gd_lic"
+
+    invalid_request = valid_request.copy()
+    invalid_request["site"] = "unsupported_site_name"
+
+    response = requests.post(url,
+                             json=invalid_request,
+                             headers=api_headers,
+                             timeout=60)
+
+    assert response.status_code == 400
+
+    data = response.json()
+
+    assert data["detail"] == f"Invalid site {invalid_request['site']}. Choose LugaggiaInnovationCommunity or GaramèDistrict."
+
+def test_request_with_invalid_start_time_format_produces_appropriate_response(api_url, api_headers, valid_request):
+    print("Testing that requests with invalid 'start_time' format produce the appropriate response")
+
+    url = f"{api_url}/forecast/gd_lic"
+
+    invalid_request = valid_request.copy()
+    invalid_request["start_time"] = "2026/09/16 10:00:00+00:00"
+
+    response = requests.post(url,
+                             json=invalid_request,
+                             headers=api_headers,
+                             timeout=60)
+
+    assert response.status_code == 400
+
+    data = response.json()
+
+    assert data["detail"] == f"Invalid start time format {invalid_request['start_time']}"
